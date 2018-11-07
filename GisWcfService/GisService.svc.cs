cdb28b84ef77a927728298322737e0318e39df7b
BORSCH-PC <BORSCH-PC@BORSCH-PC>
2018-11-07 22:42:48 +0300

    WCF Service added Empty WPF Client added

---
+++ GisWcfService/GisService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using GisData.db;
+
+namespace GisWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class GisService : IGisService
+    {
+        private string dbContext;
+        private GisDbContext gisDbContext;
+        private CityService cityService; 
+
+
+        public GisService() : base()
+        {
+            dbContext = ConfigurationManager.AppSettings.Get("db.context");
+            gisDbContext = new GisDbContext(dbContext);
+            cityService = new CityService(gisDbContext);
+
+        }
+
+        public List<City> GetAllCities()
+        {
+            return cityService.GetAllCities();
+        }
+
+        public List<Prognosis> GetAllPrognoses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Prognosis> GetPrognosesByCity(long cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GisWcfService/IGisService.cs
@@ -0,0 +1,28 @@
+﻿using GisData.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace GisWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IGisService
+    {
+        [OperationContract]
+        List<City> GetAllCities();
+
+        [OperationContract]
+        List<Prognosis> GetAllPrognoses();
+
+        [OperationContract]
+        List<Prognosis> GetPrognosesByCity(long cityId);
+
+        // TODO: Add your service operations here
+    }
+    
+}
