e99082a6201517bf310956352393f013faf9c5cf
BORSCH-PC <BORSCH-PC@BORSCH-PC>
2018-11-10 14:57:15 +0300

    Add schedule param to grabber

--- GisData/Program.cs
+++ GisData/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using GisData.db;
 using System.Configuration;
+using System;
+using System.Threading;
 
 namespace GisData
 {
@@ -9,23 +11,33 @@ namespace GisData
         static void Main(string[] args)
         {
             GisService gisService = new GisService();
-            gisService.GetLocalData(); //Загружаем данные   
+            //gisService.GetLocalData(); //Загружаем данные с локального источника  
+            gisService.GetData(); //Загружаем данные с сайта
 
             string dbContext = ConfigurationManager.AppSettings.Get("db.context");
             GisDbContext gisDbContext = new GisDbContext(dbContext);// подключаемся к БД
 
             CityService cityService = new CityService(gisDbContext);
             PrognosisService prognosisService = new PrognosisService(gisDbContext);
-            
-            foreach (KeyValuePair<long, string> city in gisService.Cities)
-            {   
-                if (cityService.FindByName(city.Value) == null){
-                    City cityObj = cityService.CreateCity(city.Key, city.Value);
-                    cityService.AddCity(cityObj);                    
+
+            do
+            {
+                // Если передаем аргумент при запуске, то считаем что это периодичность чтения данных, 
+                // иначе - проходим один раз и завершаем выполнение
+                int period = (args.Length > 0) ? Int32.Parse(args[0]): 0;
+                foreach (KeyValuePair<long, string> city in gisService.Cities)
+                {
+                    if (cityService.FindByName(city.Value) == null)
+                    {
+                        City cityObj = cityService.CreateCity(city.Key, city.Value);
+                        cityService.AddCity(cityObj);
+                    }
+                    Prognosis prognosis = PrognosisService.ParsePrognosis(city.Key, gisService.GetPrognosisData(city.Key));
+                    prognosisService.AddPrognosis(prognosis);
                 }
-                Prognosis prognosis = PrognosisService.ParsePrognosis(city.Key, gisService.GetPrognosisData(city.Key));
-                prognosisService.AddPrognosis(prognosis);
-            }
+
+                Thread.Sleep(period * 1000);
+            } while (true && args.Length > 0);            
         }
     }
 }
